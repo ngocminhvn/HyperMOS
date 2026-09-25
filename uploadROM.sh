d86067ff5500e7e3970864a5c2f7823f8c3a32ab
ngocminhvn <trinhngocminhads@gmail.com>
2026-09-26 01:44:19 +0700

    0

--- uploadROM.sh
+++ uploadROM.sh
@@ -1,7 +1,11 @@
 work_dir=$(pwd)
 source $work_dir/functions.sh
 RCLONE_CONFIG_1DRIVE="$work_dir/rclone.conf"
-ONEDRIVE_REMOTE="github"
+
+# Cấu hình Google Drive 
+GDRIVE_REMOTE="github"
+GDRIVE_FOLDER="HyperOS_ROM" 
+
 os_type=$(cat $work_dir/bin/ddevice/os_type.txt)
 base_rom_code=$(cat $work_dir/bin/ddevice/base_rom_code.txt)
 androidVER=$(cat $work_dir/bin/ddevice/androidver.txt)
@@ -11,32 +15,6 @@ device_code=$(cat $work_dir/bin/ddevice/device_code.txt)
 baserom_type=$(cat $work_dir/bin/ddevice/romtype.txt)
 device_f=$(cat $work_dir/bin/ddevice/device_f.txt)
 
-if [ "$1" == "setup" ]; then
-  if [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
-    echo "[ERROR] - Please provide rclone token and remote name"
-    exit 1
-  fi
-  curl  -s -o $work_dir/rclone.conf \
-        -H "Authorization: token $2" \
-        -H "Accept: application/vnd.github.v3.raw" \
-        -L https://api.github.com/repos/$3/contents/$4
-        
-  TOKEN_DIR=$(dirname "$4")
-  if [ "$TOKEN_DIR" == "." ]; then
-    TOKEN_PATH="token.pickle"
-  else
-    TOKEN_PATH="$TOKEN_DIR/token.pickle"
-  fi
-  
-  curl  -s -o $work_dir/token.pickle \
-        -H "Authorization: token $2" \
-        -H "Accept: application/vnd.github.v3.raw" \
-        -L https://api.github.com/repos/$3/contents/$TOKEN_PATH
-        
-  exit 0
-fi
-
-
 if [[ $(git branch --show-current) == "beta" ]]; then
     polyxver="$(cat Version)"
 	status="Development"
@@ -45,66 +23,80 @@ else
 	status="Official"
 fi
 
-if [[ $rom_os == "MIUI" ]];then
-    os_type="MIUI"
+# ========================================================
+# LOGIC NHẬN DIỆN HỆ ĐIỀU HÀNH CHUẨN XÁC 100%
+if [[ "$base_rom_code" == OS* ]]; then
+    true_os="HyperOS"
 else
-    os_type="HyperOS"
+    true_os="MIUI"
 fi
+
+# Ép tên hệ điều hành để xuất file
+os_type=$true_os
+# ========================================================
+
+repack "Compressing super.img"
+zstd --rm $work_dir/build/baserom/images/super.img -o $work_dir/build/baserom/images/super.img.zst > /dev/null 2>&1
 
 repack "Generating flashing script"
-if [[ ${baserom_type} == 'payload' ]]; then
-    mkdir -p $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/
-    mkdir -p $work_dir/out/${os_type}_${device_code}_${base_rom_code}/super/
-	mv -f $work_dir/build/baserom/images/super.img $work_dir/out/${os_type}_${device_code}_${base_rom_code}/super/
-    mv -f $work_dir/build/baserom/images/*.img $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/
-elif [[ ${baserom_type} == 'br' ]]; then
-    mkdir -p $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/
-    mv -f $work_dir/build/baserom/firmware-update/* $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/
-    mv -f $work_dir/build/baserom/images/super.img $work_dir/out/${os_type}_${device_code}_${base_rom_code}/super/
-fi
+mkdir -p $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/
 
-cp -rf $work_dir/bin/script2flash/cust.img $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/ 2>/dev/null || true
-cp -rf $work_dir/bin/script2flash/*.install $work_dir/out/${os_type}_${device_code}_${base_rom_code}/
+# Remove firmware images (user requested)
+rm -f $work_dir/build/baserom/images/{abl,xbl,xbl_config,xbl_ramdump,tz,hyp,devcfg,keymaster,qupfw,uefisecapp,modem,dsp,bluetooth,cpucp,shrm,logo,featenabler,cmnlib,cmnlib64,tzdev,storsec,aop,multiimgoem,imagefv,apdp,msadp}.img 2>/dev/null || true
+rm -f $work_dir/build/baserom/images/firmware* 2>/dev/null || true
+
+# Move compressed super
+mv -f $work_dir/build/baserom/images/super.img.zst $work_dir/out/${os_type}_${device_code}_${base_rom_code}/ 2>/dev/null || true
+
+# Move all remaining images (boot, init_boot, vendor_boot, recovery, cust, etc.)
+mv -f $work_dir/build/baserom/images/*.img $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/ 2>/dev/null || true
+
+# generate dynamic script
+cp -rf $work_dir/bin/script2flash/META-INF $work_dir/out/${os_type}_${device_code}_${base_rom_code}/
+cp -rf $work_dir/bin/script2flash/*.bat $work_dir/out/${os_type}_${device_code}_${base_rom_code}/
+cp -rf $work_dir/bin/script2flash/*.sh $work_dir/out/${os_type}_${device_code}_${base_rom_code}/
+cp -rf $work_dir/bin/script2flash/cust.img $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/
+echo $device_f > $work_dir/out/${os_type}_${device_code}_${base_rom_code}/META-INF/Data/DeviceCode
+repack "Done"
+
 
 find out/${os_type}_${device_code}_${base_rom_code} |xargs touch
 pushd out/${os_type}_${device_code}_${base_rom_code}/ || exit
 zip -r ${os_type}_${device_code}_${base_rom_code}.zip ./*
 mv ${os_type}_${device_code}_${base_rom_code}.zip ../
 popd || exit
-hash=$(md5sum out/${os_type}_${device_code}_${base_rom_code}.zip |head -c 5)
-mv out/${os_type}_${device_code}_${base_rom_code}.zip out/${os_type}_${polyxver}_${device_code}_${base_rom_code}_${hash}_${status}.zip
+current_date=$(date +"%d%m%y")
+mv out/${os_type}_${device_code}_${base_rom_code}.zip out/${os_type}_${polyxver}_${device_code}_${base_rom_code}_${current_date}_${status}.zip
 repack "Build completed"    
 repack "Output: "
-repack "$(pwd)/out/${os_type}_${polyxver}_${device_code}_${base_rom_code}_${hash}_${status}.zip"
+repack "$(pwd)/out/${os_type}_${polyxver}_${device_code}_${base_rom_code}_${current_date}_${status}.zip"
 upload "Uploading"
-output_file="out/${os_type}_${polyxver}_${device_code}_${base_rom_code}_${hash}_${status}.zip"
-echo "${os_type}_${polyxver}_${device_code}_${base_rom_code}_${hash}_${status}.zip" > $work_dir/bin/ddevice/output_zip.txt
+output_file="out/${os_type}_${polyxver}_${device_code}_${base_rom_code}_${current_date}_${status}.zip"
+echo "${os_type}_${polyxver}_${device_code}_${base_rom_code}_${current_date}_${status}.zip" > $work_dir/bin/ddevice/output_zip.txt
 
-if [[ $rom_os == "MIUI" ]];then
-    uploaddir="MIUI"
-else
-    uploaddir="HyperOS"
-fi
+# Đặt tên thư mục upload trên Drive trùng với hệ điều hành
+uploaddir=$true_os
 
-# 1drive
-# if [[ $rom_os == "MIUI" ]]; then
-#     rclone -v --config="$RCLONE_CONFIG_1DRIVE" copy "$output_file" "$ONEDRIVE_REMOTE:NTBuild/${uploaddir}/${polyxver}/${device_code}/" || {
-#         upload "Error uploading file to OneDrive: $FILENAME"
-#         exit 1
-#     }
-# else
-#     rclone -v --config="$RCLONE_CONFIG_1DRIVE" copy "$output_file" "$ONEDRIVE_REMOTE:NTBuild/${uploaddir}/${polyxver}/${device_code}/" || {
-#         upload "Error uploading file to OneDrive: $FILENAME"
-#         exit 1
-#     }
-# fi  
-
-# Google Drive
+# Upload thẳng lên Google Drive (Đã tối ưu rclone chống kẹt 100%)
 upload "Uploading to Google Drive..."
-python $work_dir/upload_rom_api.py "$output_file" --folder_id "1aJw0uWIMD7Yc1n4O1mjbI0YLHjRevfxl" --path "${uploaddir}/${polyxver}/${device_code}/" || {
-    upload "Error uploading file to Google Drive"
+
+rclone -v --config="$RCLONE_CONFIG_1DRIVE" copy "$output_file" "$GDRIVE_REMOTE:$GDRIVE_FOLDER/${uploaddir}/${polyxver}/${device_code}/" \
+    --drive-chunk-size 128M \
+    --tpslimit 4 \
+    --retries 3 \
+    --timeout 15m \
+    --contimeout 15m || {
+    upload "Lỗi khi upload file lên Google Drive!"
+    exit 1
 }
+remote_file_path="$GDRIVE_REMOTE:$GDRIVE_FOLDER/${uploaddir}/${polyxver}/${device_code}/$(basename "$output_file")"
+share_link=$(rclone --config="$RCLONE_CONFIG_1DRIVE" link "$remote_file_path")
+mkdir -p bin/ddevice
+echo "$share_link" > bin/ddevice/rom_link.txt
 
 upload "Clean Workflow.."
+rm -rf $work_dir/out
+rm -rf $work_dir/build
 
 upload "Build ${os_type}_${polyxver} for ${device_code} successfull!"
+
