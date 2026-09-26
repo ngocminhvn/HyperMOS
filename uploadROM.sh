ac57f16a0e4d9362d7f2eb6e985f54cedb4a60a6
Trinh Ngoc Minh <90078397+ngocminhvn@users.noreply.github.com>
2026-09-26 11:59:39 +0700

    update uploadROM.sh

--- uploadROM.sh
+++ uploadROM.sh
@@ -1,7 +1,6 @@
 work_dir=$(pwd)
 source $work_dir/functions.sh
 RCLONE_CONFIG_1DRIVE="$work_dir/rclone.conf"
-
 if [ -z "$RCLONE_TOKEN_PATH" ]; then
     echo "Lỗi: Không tìm thấy biến môi trường RCLONE_TOKEN_PATH!"
     exit 1
@@ -17,6 +16,19 @@ fi
 # Cấu hình Google Drive 
 GDRIVE_REMOTE="github"
 GDRIVE_FOLDER="HyperOS_ROM" 
+
+# Setup Rclone Config qua API (Tương thích với bước Setup trong file YML)
+if [ "$1" == "setup" ]; then
+  if [ -z "$2" ] || [ -z "$3" ] || [ -z "$4" ]; then
+    echo "[ERROR] - Please provide rclone token and remote name"
+    exit 1
+  fi
+  curl  -s -o $work_dir/rclone.conf \
+        -H "Authorization: token $2" \
+        -H "Accept: application/vnd.github.v3.raw" \
+        -L https://api.github.com/repos/$3/contents/$4
+  exit 0
+fi
 
 os_type=$(cat $work_dir/bin/ddevice/os_type.txt)
 base_rom_code=$(cat $work_dir/bin/ddevice/base_rom_code.txt)
@@ -37,37 +49,43 @@ fi
 
 # ========================================================
 # LOGIC NHẬN DIỆN HỆ ĐIỀU HÀNH CHUẨN XÁC 100%
-if [[ "$base_rom_code" == OS* ]]; then
-    true_os="HyperOS"
-else
+if [[ $rom_os == "MIUI" ]] || [[ "$base_rom_code" != OS* ]]; then
+    os_type="MIUI"
     true_os="MIUI"
+    uploaddir="MIUI"
+else
+    os_type="HyperOS"
+    true_os="HyperOS"
+    uploaddir="HyperOS"
 fi
-
-# Ép tên hệ điều hành để xuất file
-os_type=$true_os
 # ========================================================
 
 repack "Compressing super.img"
 zstd --rm $work_dir/build/baserom/images/super.img -o $work_dir/build/baserom/images/super.img.zst > /dev/null 2>&1
 
 repack "Generating flashing script"
-mkdir -p $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/
 
-# Remove firmware images (user requested)
+# Xóa các file firmware không mong muốn (user requested)
 rm -f $work_dir/build/baserom/images/{abl,xbl,xbl_config,xbl_ramdump,tz,hyp,devcfg,keymaster,qupfw,uefisecapp,modem,dsp,bluetooth,cpucp,shrm,logo,featenabler,cmnlib,cmnlib64,tzdev,storsec,aop,multiimgoem,imagefv,apdp,msadp}.img 2>/dev/null || true
 rm -f $work_dir/build/baserom/images/firmware* 2>/dev/null || true
 
-# Move compressed super
-mv -f $work_dir/build/baserom/images/super.img.zst $work_dir/out/${os_type}_${device_code}_${base_rom_code}/ 2>/dev/null || true
+# Tạo thư mục và sao chép an toàn (Khắc phục lỗi mất file images)
+mkdir -p $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/
 
-# Move all remaining images (boot, init_boot, vendor_boot, recovery, cust, etc.)
-mv -f $work_dir/build/baserom/images/*.img $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/ 2>/dev/null || true
+if [[ ${baserom_type} == 'payload' ]]; then
+    mv -f $work_dir/build/baserom/images/super.img.zst $work_dir/out/${os_type}_${device_code}_${base_rom_code}/ 2>/dev/null || true
+    mv -f $work_dir/build/baserom/images/*.img $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/ 2>/dev/null || true
+elif [[ ${baserom_type} == 'br' ]]; then
+    mv -f $work_dir/build/baserom/firmware-update/* $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/ 2>/dev/null || true
+    mv -f $work_dir/build/baserom/images/super.img.zst $work_dir/out/${os_type}_${device_code}_${base_rom_code}/ 2>/dev/null || true
+fi
 
 # generate dynamic script
-cp -rf $work_dir/bin/script2flash/META-INF $work_dir/out/${os_type}_${device_code}_${base_rom_code}/
-cp -rf $work_dir/bin/script2flash/*.bat $work_dir/out/${os_type}_${device_code}_${base_rom_code}/
-cp -rf $work_dir/bin/script2flash/*.sh $work_dir/out/${os_type}_${device_code}_${base_rom_code}/
-cp -rf $work_dir/bin/script2flash/cust.img $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/
+cp -rf $work_dir/bin/script2flash/META-INF $work_dir/out/${os_type}_${device_code}_${base_rom_code}/ 2>/dev/null || true
+cp -rf $work_dir/bin/script2flash/*.bat $work_dir/out/${os_type}_${device_code}_${base_rom_code}/ 2>/dev/null || true
+cp -rf $work_dir/bin/script2flash/*.sh $work_dir/out/${os_type}_${device_code}_${base_rom_code}/ 2>/dev/null || true
+cp -rf $work_dir/bin/script2flash/*.install $work_dir/out/${os_type}_${device_code}_${base_rom_code}/ 2>/dev/null || true
+cp -rf $work_dir/bin/script2flash/cust.img $work_dir/out/${os_type}_${device_code}_${base_rom_code}/images/ 2>/dev/null || true
 echo $device_f > $work_dir/out/${os_type}_${device_code}_${base_rom_code}/META-INF/Data/DeviceCode
 repack "Done"
 
@@ -77,6 +95,7 @@ pushd out/${os_type}_${device_code}_${base_rom_code}/ || exit
 zip -r ${os_type}_${device_code}_${base_rom_code}.zip ./*
 mv ${os_type}_${device_code}_${base_rom_code}.zip ../
 popd || exit
+
 current_date=$(date +"%d%m%y")
 mv out/${os_type}_${device_code}_${base_rom_code}.zip out/${os_type}_${polyxver}_${device_code}_${base_rom_code}_${current_date}_${status}.zip
 repack "Build completed"    
@@ -86,8 +105,6 @@ upload "Uploading"
 output_file="out/${os_type}_${polyxver}_${device_code}_${base_rom_code}_${current_date}_${status}.zip"
 echo "${os_type}_${polyxver}_${device_code}_${base_rom_code}_${current_date}_${status}.zip" > $work_dir/bin/ddevice/output_zip.txt
 
-# Đặt tên thư mục upload trên Drive trùng với hệ điều hành
-uploaddir=$true_os
 
 # Upload thẳng lên Google Drive (Đã tối ưu rclone chống kẹt 100%)
 upload "Uploading to Google Drive..."
@@ -111,4 +128,3 @@ rm -rf $work_dir/out
 rm -rf $work_dir/build
 
 upload "Build ${os_type}_${polyxver} for ${device_code} successfull!"
-
