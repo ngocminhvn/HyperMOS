366597e1668052e8c32e934bda6f7ec09bbe2098
ngocminhvn <trinhngocminhads@gmail.com>
2026-09-26 14:29:44 +0700

    ok

--- notify.py
+++ notify.py
@@ -711,7 +711,7 @@ def send_notification(status, repo_name, rom_link, channel_id, bot_token, msg_id
                 gdrive_link = read_file_if_exists("bin/ddevice/rom_link.txt")
                 if not gdrive_link:
                     gdrive_link = "https://google.com"
-                pm_lines.extend(["", "<b>Tải ROM:</b> <a href=\"{gdrive_link}\">Link</a>"])
+                pm_lines.extend(["", f"<b>Tải ROM:</b> <a href=\"{gdrive_link}\">{gdrive_link}</a>"])
             elif status == "fail":
                 pm_lines.extend(["", "<b>Log lỗi:</b> file .txt sẽ được gửi riêng ngay sau tin nhắn này nếu tìm thấy log."])
             else:
